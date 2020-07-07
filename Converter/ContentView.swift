0ff32eee530423efbf3ce9fdc37c0a3562860b41
obuseme <obusek@gmail.com>
2020-07-07 16:29:48 -0400

    Initial Commit

---
+++ Converter/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  Converter
+//
+//  Created by Andrew Obusek on 7/7/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!").padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
